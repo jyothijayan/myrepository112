2380d9fc3b99e0fe33777a3a035812ae02985b32
Jyothi Jayan <jyothijayan111@gmail.com>
2021-10-09 18:08:12 +0530

    Added new class

---
+++ src/test/java/com/myapp/qa/test/Myappclass.java
@@ -0,0 +1,34 @@
+package com.myapp.qa.test;
+
+import org.testng.annotations.Test;
+
+public class Myappclass {
+	/*vvvvvvb*/
+	
+	@Test
+	public void firsttest()
+	{
+		System.out.println("Hai jyothi1");
+	}
+	@Test
+	public void firsttest2()
+	{
+		System.out.println("Hai jyothi2");
+	}
+	@Test
+	public void firsttest3()
+	{
+		System.out.println("Hai jyothi3");
+	}
+	@Test
+	public void firsttest4()
+	{
+		System.out.println("Hai jyothi4");
+	}
+	@Test
+	public void firsttest5()
+	{
+		System.out.println("Hai jyothi5");
+	}
+
+}
